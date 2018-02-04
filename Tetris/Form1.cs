44733eeb51b6b2c44109f72fcfa03deac0b4d0a3
stzbilj <s.zbiljski@gmail.com>
2018-02-04 23:31:16 +0100

    Add project files.

---
+++ Tetris/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        //ubaciti timer i funkciju koja reagira na event
+        private Button[,] buttonArray;
+        private TetrisField tField;
+
+        public Form1()
+        {
+            buttonArray = new Button[20, 10];
+            tField = new TetrisField();
+
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    buttonArray[i, j] = new Button();
+                    buttonArray[i, j].Size = new Size(30, 30);
+                    buttonArray[i, j].Location = new Point(31*j + 1, 31*i + 1);
+                    buttonArray[i, j].Enabled = false;
+                    //color will depend on Teteris field
+                    switch (tField.GetType(i, j))
+                    {
+                        case 0:
+                            buttonArray[i, j].BackColor = Color.Red;
+                            break;
+                        case 1:
+                            buttonArray[i, j].BackColor = Color.Blue;
+                            break;
+                        case 2:
+                            buttonArray[i, j].BackColor = Color.Yellow;
+                            break;
+                        default:
+                            buttonArray[i, j].BackColor = Color.Black;
+                            break;
+
+                    }
+                    buttonArray[i, j].TabStop = false;
+                    buttonArray[i, j].FlatStyle = FlatStyle.Flat;
+                    buttonArray[i, j].FlatAppearance.BorderSize = 0;
+                    this.Controls.Add(buttonArray[i, j]);
+                }
+            this.BackColor = Color.Aqua;
+            this.Size = new Size(30 * 20, 30 * 30);
+            InitializeComponent();
+        }
+
+        
+    }
+
+    
+
+    /*public class TetrisObject
+    {
+        private int[,] tObject;
+        
+    }*/
+}
---
+++ Tetris/TetrisField.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class TetrisField
+    {
+        //0 empty, 1 block, 2 moving object 
+        private int[,] matrix;
+
+        public TetrisField()
+        {
+            matrix = new int[22, 10];
+            //field is empty at start
+            for (int i = 0; i < 22; ++i)
+            {
+                for (int j = 0; j < 10; ++j)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        private bool IsRowFull(int row)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                if (matrix[row, i] == 0 || matrix[row, i] == 2)
+                    return false;
+            }
+            return true;
+        }
+
+        public int GetType(int row, int column)
+        {
+            return matrix[row + 2, column];
+        }
+
+
+    }
+}
